3ce31ffd7087f48ab81967e633d7aa5f8a9299d6
Ateeb karim <ateebkarim.ak02@gmail.com>
2026-07-11 21:32:18 +0500

    Update ThemeContext.jsx

--- src/Context/ThemeContext.jsx
+++ src/Context/ThemeContext.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const themeContext = React.createContext({
+export const ThemeContext = React.createContext({
     themeMode: "light",
     darkTheme: () => { },
     lightTheme: () => { },
